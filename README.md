4437b0b7479ad12a83b10c34fa91cffc7c51fbcb
Mariusz Zalewski <mariusz@zalewscy.eu>
2017-01-24 23:10:05 +0100

    puppet ocnfig for 2nodes corrrected

--- README.md
+++ README.md
@@ -1,17 +1,29 @@
 # vagrant-linux-labs
 Set of Vagrant configuration for Linux Labs
 
-# Tested on Environments
+## Tested on Environments
 
-ENV1
-Vagrant 1.9.1 & Oracle VirtualBox 5.1.12r112440 @ Arch Linux - 4.8.13-1-ARCH #1 SMP PREEMPT Fri Dec 9 07:24:34 CET 2016 x86_64 GNU/Linux
+1. ENV1
+ * Vagrant 1.9.1
+ * Oracle VirtualBox 5.1.12r112440
+ * Arch Linux - 4.8.13-1-ARCH #1 SMP PREEMPT Fri Dec 9 07:24:34 CET 2016 x86_64 GNU/Linux
 
-ENV2
-Vagrant 1.9.1 & Oracle VirtualBox 5.1.14r112924 @ Linux Mint 18 Sarach - 4.4.0-59-generic #80-Ubuntu SMP Fri Jan 6 17:47:47 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux
+2. ENV2
+ * Vagrant 1.9.1
+ * Oracle VirtualBox 5.1.14r112924
+ * Linux Mint 18 Sarah - 4.4.0-59-generic #80-Ubuntu SMP Fri Jan 6 17:47:47 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux
 
-# Test Results
+## Test Results
+
 VagrantDirConfig; 			ENV1 Result; 	ENV2 Result;
-xm-lab-provisioned-by-puppet-1node; 	PASS; 		NOT TESTED;
-xm-lab-provisioned-by-puppet-2nodes; 	PASS;		NOT TESTED;
-xm-lab-ssh;				PASS;		PASS;
+
+1. xm-lab-ssh
+ * EVN1 OK
+ * ENV2 OK
+2. xm-lab-provisioned-by-puppet-1node
+ * ENV1 OK
+ * ENV2 Not tested
+3. xm-lab-provisioned-by-puppet-2nodes
+ * ENV1 OK
+ * ENV2 Not tested
 
--- xm-lab-provisioned-by-puppet-2nodes/manifests/default.pp
+++ xm-lab-provisioned-by-puppet-2nodes/manifests/default.pp
@@ -1,22 +1,52 @@
-exec { "apt-get update":
-	command	=> "/usr/bin/apt-get update",
+#Puppet configuration for Vagrant hosts
+
+
+class hostsfile {
+	#this class provides proper /etc/hosts for all machines
+	#no DNS server needed ;-)
+
+	host { 'host01': ip => '192.168.77.201', host_aliases => [ 'server', 'srv' ], }	
+  	host { 'host02': ip => '192.168.77.202', host_aliases => [ 'client', 'cli' ], }	
+  	
+	#this is only example that you can add external IPs to /etc/hosts
+	host { 'googledns': ip => '8.8.8.8', host_aliases => [ 'g1', 'google1' ], }
+
+	# ;-)
+	host { 'www.onet.pl': ip => '127.0.0.1', host_aliases => [ 'onet.pl', 'onet' ], }	
+
+	#below line is only for fast copying
+	#host { '': ip => '192.168.77.', host_aliases => [ 'client', 'cli' ], }	
 }
 
-file { "xonicman.box":
-	path	=> "/usr/local/lib/xonicman.box",
-	ensure	=> directory,
-}
+class baseconfig {
+	include hostsfile
 
-#packages to install:
-package { "htop": ensure => installed, require => Exec["apt-get update"], }
-package { "vim": ensure => installed, require => Exec["apt-get update"], }
-package { "tree": ensure => installed, require => Exec["apt-get update"], }
+	exec { "apt-get update":
+		command	=> "/usr/bin/apt-get update",
+	}
+
+	file { "xonicman.box":
+		path	=> "/usr/local/lib/xonicman.box",
+		ensure	=> directory,
+	}
+
+	#packages to install:
+	package { "htop": ensure => installed, require => Exec["apt-get update"], }
+	package { "vim": ensure => installed, require => Exec["apt-get update"], }
+	package { "tree": ensure => installed, require => Exec["apt-get update"], }
+	#package { "": ensure => installed, require => Exec["apt-get update"], }
+
+}
 
 
 node host01 {
+	include baseconfig
 	package { "iotop": ensure => installed, require => Exec["apt-get update"], }
 }
 
+
 node host02 {
+	include baseconfig
 	package { "nmap": ensure => installed, require => Exec["apt-get update"], }
 }
+
